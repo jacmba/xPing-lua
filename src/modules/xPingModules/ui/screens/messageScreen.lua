74aac50f8066f89a838659923c869cc32a713076
Jacinto Mba Cantero <jacmba@gmail.com>
2025-01-23 16:40:34 +0000

    🚧 Implementing scrollable area for message list

--- src/modules/xPingModules/ui/screens/messageScreen.lua
+++ src/modules/xPingModules/ui/screens/messageScreen.lua
@@ -3,18 +3,21 @@
 local messageScreen = {}
 
 function messageScreen.show()
-	imgui.TextUnformatted("Received messages")
-	imgui.TextUnformatted("")
-	
-	for _, msg in ipairs(App.messages) do
-		local message = msg.message
-		if imgui.TreeNode(message.title) then
-			imgui.TextUnformatted(message.content)
+	imgui.TextUnformatted("RECEIVED MESSAGES")
+	if imgui.BeginChild("Received messages", 350, 200, true) then
+		for _, msg in ipairs(App.messages) do
+			local message = msg.message
+			if imgui.TreeNode(message.title) then
+				imgui.TextUnformatted(message.content)
+			end
 		end
+		
+		imgui.TextUnformatted("")
+		imgui.TextUnformatted("")
 	end
 	
-	imgui.TextUnformatted("")
-	imgui.TextUnformatted("")
+	imgui.EndChild()
+	
 	if imgui.Button("Back to menu") then
 		App.goToMenu()
 	end
